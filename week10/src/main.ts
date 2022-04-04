f5d51fe6f1380947c1ef4279b4715397240f005f
jenna5376 <jenna5376@gmail.com>
2022-04-04 01:04:39 -0400

    update

--- week10/src/main.ts
+++ week10/src/main.ts
@@ -19,6 +19,7 @@ let stats: any;
 let plane: THREE.Mesh;
 let group: THREE.Group;
 let roomModel: THREE.Group;
+let cube: THREE.Mesh;
 
 
 import vertexShader from '../resources/shaders/shader.vert?raw';
@@ -38,10 +39,8 @@ function initStats() {
 
 function initScene() {
     scene = new THREE.Scene();
-
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	camera.position.set( 10, 10, 10 );
-
 
     renderer = new THREE.WebGLRenderer();
     renderer.shadowMap.enabled = true;
@@ -56,50 +55,43 @@ function initScene() {
    
     renderer.shadowMap.enabled = true;
 
-	//ref https://www.youtube.com/watch?v=a0qSHBnqORU
-	const rayCaster = new THREE.Raycaster();
-	const clickMouse = new THREE.Vector2();
-	const moveMouse = new THREE.Vector2();
-	var draggable: THREE.Object3D;
+	/*i
+	
+	my first idea was using a raycaster to enable users to pick up/move furnitures but it didn't work
+	*perhaps because i was using 3d groups/children rather than just 3d objects?
+	reference code: https://www.youtube.com/watch?v=a0qSHBnqORU&t=235s 
+	
+	*/
+	const floor = 0.9;
 
-
-
-
-    //create mesh for ice cream
     const carpetMat = new THREE.MeshToonMaterial({color: 0xA495CB})
 	const wallMat = new THREE.MeshToonMaterial({color: 0x7CA6B7})
     const floorMat = new THREE.MeshToonMaterial({color: 0xA99388})
     const deskMat = new THREE.MeshToonMaterial({color: 0x816863})
-
-	//bed
 	const whiteMat = new THREE.MeshToonMaterial({color: 0xEAE0E0})
     const sheet1Mat = new THREE.MeshToonMaterial({color: 0x575591})
     const cushionMat = new THREE.MeshToonMaterial({color: 0x575591})
 	const frameMat = new THREE.MeshToonMaterial({color: 0x816863});
-
     const starMat = new THREE.MeshToonMaterial({color: 0xF3A3C5})
 	const paintingMat = new THREE.MeshToonMaterial({color: 0xBBF1F1});
-
-
 	const potMat = new THREE.MeshToonMaterial({color: 0x8C3B3B});
     const grassMat = new THREE.MeshToonMaterial({color: 0x518046})
 	const barkMat = new THREE.MeshToonMaterial({color: 0x48382F});
-
     const clockMat = new THREE.MeshToonMaterial({color: 0x2B3966})
 	const trashMat = new THREE.MeshToonMaterial({color: 0xA26464})
-
 	const laptopMat = new THREE.MeshToonMaterial({color: 0xAFAFB2});
+	const geometryBox = new THREE.BoxGeometry(1,1,1);
+    const materialBox = new THREE.MeshToonMaterial({ color: 0x56768f});
+    
+	//cube player
+	cube = new THREE.Mesh(geometryBox, materialBox);
+	cube.position.z = 5;
+	cube.position.y = 0.9
+	cube.position.x = 5
 
-/*
-
-	window.addEventListener("click", event => {
-		clickMouse.x = (event.clientX / window.innerWidth)*2 -1;
-		clickMouse.y = - (event.clientY / window.innerHeight)*2+1;
-
-		rayCaster.setFromCamera( clickMouse, camera);
-	})
-
-	*/
+    cube.castShadow = true;
+    cube.receiveShadow = true; 
+    scene.add(cube);
 
 	//add ambient light
 	const light = new THREE.AmbientLight( 0x404040 ); // soft white light
@@ -108,17 +100,16 @@ function initScene() {
 	const light2 = new THREE.AmbientLight( 0x404040, 0.9 ); // soft white light
 	scene.add( light2 )
 
-    //ice cream 1
+	scene.add(cube);
+
+    //add room
 	group = new THREE.Group()
 	scene.add(group)
 
         const modelLoader = new GLTFLoader().setPath('../resources/models/');
 		modelLoader.load('room.gltf', (gltf) => {
 			roomModel = gltf.scene;
-
 			roomModel.scale.set(0.01,0.01,0.01);
-  
-
 
 			interface gltfMesh extends THREE.Object3D<THREE.Event> {
 				material: THREE.Material
@@ -177,14 +168,8 @@ function initScene() {
                     (child as gltfMesh).material = laptopMat;
                 }
             })
-
-            group.rotateY(0.3);
 			group.add(roomModel)
 		});
-        
-        //not sure why but adding shadow caused the whole scene to turn black
-        //roomModel.castShadow = true;
-
 
     const uniforms = {
         u_time: { type: 'f', value: 1.0 },
@@ -208,29 +193,39 @@ function initListeners() {
     window.addEventListener('keydown', (event) => {
         const { key } = event;
 
+				if (key == 'w' && cube.position.x > 1){
+					cube.position.x -=1;
+				}
+				else if (key == 's' && cube.position.x < 7){
+					cube.position.x +=1;
+				}
+				else if (key == 'a' && cube.position.z < 7){
+					cube.position.z +=1;
+				}
+				else if (key == 'd' && cube.position.z > 1){
+					cube.position.z -=1;
+				}
+				else if (cube.position.y <= .9 && key == ' '){
+					cube.position.y +=1.5;
+				}
+			
+
+		/* resulted in only s, d input working
         switch (key) {
-            case 'e':
-                const win = window.open('', 'Canvas Image');
-
-                const { domElement } = renderer;
-
-                // Makse sure scene is rendered.
-                renderer.render(scene, camera);
-
-                const src = domElement.toDataURL();
-
-                if (!win) return;
-
-                win.document.write(`<img src='${src}' width='${domElement.width}' height='${domElement.height}'>`);
-                break;
-
+			case 'w':
+				cube.position.x -=1;
+			case 's':
+				cube.position.x+=1;
+			case 'a':
+				cube.position.z +=1;
+			case 'd':
+				cube.position.z -=1;
             default:
                 break;
         }
+		*/
     });
 }
-
-
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -239,6 +234,13 @@ function onWindowResize() {
 }
 
 function animate() {
+
+	const gravity = -0.04;
+
+	if (cube.position.y > .9){
+		cube.position.y += gravity
+	}
+
     requestAnimationFrame(() => {
         animate();
     });
